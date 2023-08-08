6713abcdd63709986ed795fa668b970b9ba4e80d
Lautaro Barba <lau_mb94@hotmail.com>
2023-08-08 20:09:23 -0300

    Sync

--- frontend/app/src/wrappers/LoadingPageWrapper.tsx
+++ frontend/app/src/wrappers/LoadingPageWrapper.tsx
@@ -6,25 +6,22 @@ import { LangContext } from "@/providers/LanguageProvider";
 import Logo from "@/assets/images/logo-circulo.png";
 
 const LoadingPage = () => {
-  const { lang } = useContext(LangContext);
   return (
-    <section className="bg-white h-screen">
+    <section id="loading" className="bg-white h-screen">
+      <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Jardín botánico de ushuaia
+      </h1>
+
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+          {/* <h1 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
             Jardín botánico de ushuaia
-          </h1>
+          </h1> */}
           <br />
-          {lang == "es" && (
-            <h2 className="text-gray-500 sm:text-xl dark:text-gray-400">
-              CARGANDO...
-            </h2>
-          )}
-          {lang == "en" && (
-            <h2 className="text-gray-500 sm:text-xl dark:text-gray-400">
-              LOADING...
-            </h2>
-          )}
+          <h2 className="text-gray-500 sm:text-xl dark:text-gray-400">
+            CARGANDO...
+          </h2>
+
           <br />
           <br />
           <div className="text-center">
@@ -57,5 +54,6 @@ export const LoadingPageWrapper = (props: LoadingPageWrapperProps) => {
   const { children } = props;
   const { loading: loadingTheme } = useContext(ThemeContext);
   const { loading: loadingLang } = useContext(LangContext);
-  return <>{loadingTheme || loadingLang ? <LoadingPage /> : <>{children}</>}</>;
+  // return <>{loadingTheme || loadingLang ? <LoadingPage /> : <>{children}</>}</>;
+  return <>{true ? <LoadingPage /> : <>{children}</>}</>;
 };
