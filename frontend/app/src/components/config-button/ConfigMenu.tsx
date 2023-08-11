9e2cada68fa8fe36744212e4dc421db2ba1dff47
Lautaro Barba <lau_mb94@hotmail.com>
2023-08-10 21:12:12 -0300

    Se arreglo bug en menu de lenguaje

--- frontend/app/src/components/config-button/ConfigMenu.tsx
+++ frontend/app/src/components/config-button/ConfigMenu.tsx
@@ -2,49 +2,67 @@
 import { useContext, useState } from "react";
 import { ThemeContext } from "@/providers/ThemeProvider";
 import { LangContext } from "@/providers/LanguageProvider";
-import {
-    Dropdown,
-    DropdownTrigger,
-    DropdownMenu,
-    DropdownSection,
-    DropdownItem,
-    Button
-} from "@nextui-org/react";
-import { MoonIcon, SunIcon } from "lucide-react";
+import { Button } from "@nextui-org/react";
+import { LanguagesIcon, MoonIcon, SunIcon } from "lucide-react";
 
 export const ConfigMenu = () => {
     const { theme, toggleTheme } = useContext(ThemeContext);
     const { lang, setLang } = useContext(LangContext);
+    const [langMenuExpand, setLangMenuExpand] = useState<boolean>(false);
+
+    const toggleLangMenu = () => {
+        setLangMenuExpand(!langMenuExpand);
+    }
 
     return (
         <div id="config-menu" className="fixed right-5 bottom-5">
+            {/* Menu de lenguajes */}
+            <div className={`ml-7 z-10 ${langMenuExpand ? 'block' : 'hidden'} bg-light dark:bg-dark  rounded-lg shadow w-10 text-center`}>
+                <Button
+                    isIconOnly
+                    type="button"
+                    size="sm"
+                    className="h-8 bg-light dark:bg-dark p-1"
+                    onClick={() => {
+                        setLang("es");
+                        setLangMenuExpand(false);
+                    }}
+                ><span className="mx-1 text-dark dark:text-light text-lg">ES</span></Button>
+                <hr />
+                <Button
+                    isIconOnly
+                    type="button"
+                    size="sm"
+                    className="h-8 bg-light dark:bg-dark p-1"
+                    onClick={() => {
+                        setLang("en");
+                        setLangMenuExpand(false);
+                    }}
+                ><span className="mx-1 text-dark dark:text-light text-lg">EN</span></Button>
+            </div>
+            {/* Toggle Buttons */}
             <div className="flex flex-row items-center">
-                <Button isIconOnly type="button" size="sm" onClick={toggleTheme} className="h-8 bg-light dark:bg-dark p-1">
+                <Button
+                    isIconOnly
+                    type="button"
+                    size="sm"
+                    className="h-8 bg-light dark:bg-dark p-1"
+                    onClick={toggleTheme}
+                >
                     <span className="mx-1 text-dark dark:text-light">{theme === "light" ? <MoonIcon /> : <SunIcon />}</span>
                 </Button>
 
-                <Dropdown className="h-auto">
-                    <DropdownTrigger>
-                        <Button isIconOnly type="button" size="sm" className="h-8 bg-light dark:bg-dark p-1 uppercase font-semibold">
-                            <span className="mx-1 text-dark dark:text-light">{lang.toLocaleUpperCase()}</span>
-                        </Button>
-                    </DropdownTrigger>
-                    <DropdownMenu
-                        aria-label="language-selector"
-                        variant="solid"
-                        className="text-dark dark:text-light bg-light dark:bg-dark hover:bg-red w-auto"
-                    // TODO: Arreglar el ancho de las opciones
-                    // style={{ width: '1rem !important' }}
-                    >
-                        <DropdownItem key="ES" onClick={() => setLang("es")} className="text-dark dark:text-light bg-light dark:bg-dark hover:bg-red w-auto">
-                            ES
-                        </DropdownItem>
-                        <DropdownItem key="EN" onClick={() => setLang("en")} className="text-dark dark:text-light bg-light dark:bg-dark hover:bg-red w-auto">
-                            EN
-                        </DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-            </div>
-        </div>
+                <Button
+                    isIconOnly
+                    type="button"
+                    size="sm"
+                    className="h-8 bg-light dark:bg-dark p-1"
+                    onClick={toggleLangMenu}
+                >
+                    {!langMenuExpand && (<span className="mx-1 text-dark dark:text-light text-lg">{lang.toLocaleUpperCase()}</span>)}
+                    {langMenuExpand && (<span className="mx-1 text-dark dark:text-light"><LanguagesIcon /></span>)}
+                </Button>
+            </div >
+        </div >
     );
 }
