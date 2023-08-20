5577c3658b4153cf4a46039ea2abe429bc3f022b
Lautaro Barba <lau_mb94@hotmail.com>
2023-08-20 17:41:25 -0300

    Se agregaron iconos en sidebar

--- react/app/src/layouts/PrivateLayout.tsx
+++ react/app/src/layouts/PrivateLayout.tsx
@@ -10,7 +10,7 @@ export const PrivateLayout = () => {
   const isAuthenticated = useUserIsAuthenticated();
   const location = useLocation();
   const user = useCurrentUser();
-  
+
   const getSideBarLastState = () => {
     const sideBarData: string | null = localStorage.getItem("sidebar");
     if (sideBarData) {
@@ -31,15 +31,15 @@ export const PrivateLayout = () => {
     localStorage.setItem("sidebar", JSON.stringify(!prevState));
   };
 
-  // Valido si el usuario esta autenticado
-  if(!isAuthenticated){
-    return (<Navigate to="/app/auth/login" replace state={{ location }}/>); 
-  }
+  // // Valido si el usuario esta autenticado
+  // if(!isAuthenticated){
+  //   return (<Navigate to="/app/auth/login" replace state={{ location }}/>); 
+  // }
 
-  // Valido si el usuario confirmó su correo electrónico
-  if(user && !user.isEmailConfirmed){
-    return (<Navigate to="/app/auth/email-confirmation-required" replace state={{ location }}/>);
-  }
+  // // Valido si el usuario confirmó su correo electrónico
+  // if(user && !user.isEmailConfirmed){
+  //   return (<Navigate to="/app/auth/email-confirmation-required" replace state={{ location }}/>);
+  // }
 
   return (
     <>
@@ -53,10 +53,10 @@ export const PrivateLayout = () => {
         menuState={menuState}
       />
       <div className="d-flex flex-row">
-        <div 
-          id="sidebar-spacer" 
-          className="d-none d-md-block" 
-          style={{ backgroundColor: 'red', minWidth: menuState ? DRAWERWIDTH: 0, transitionDuration: '0.2s'}}
+        <div
+          id="sidebar-spacer"
+          className="d-none d-md-block"
+          style={{ backgroundColor: 'red', minWidth: menuState ? DRAWERWIDTH : 0, transitionDuration: '0.2s' }}
         ></div>
         <div className="w-100">
           <div id="navbar-spacer" style={{ height: "65px" }}></div>
