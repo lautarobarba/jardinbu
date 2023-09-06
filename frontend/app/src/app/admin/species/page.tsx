a3da9f474f7dac5b0e858b488f40b977896015e3
Lautaro Barba <lau_mb94@hotmail.com>
2023-09-06 14:55:46 -0300

    Se cambio la tabla de especies

--- frontend/app/src/app/admin/species/page.tsx
+++ frontend/app/src/app/admin/species/page.tsx
@@ -5,13 +5,14 @@ import { Role } from "@/interfaces/user.interface";
 import { useGetSpecies } from "@/services/hooks";
 import { ModalThemeWrapper } from "@/wrappers/ModalThemeWrapper";
 import { RolRequiredPageWrapper } from "@/wrappers/RolRequiredPageWrapper";
-import { Button, CircularProgress, Modal, ModalContent } from "@nextui-org/react";
+import { Button, CircularProgress, Input, Modal, ModalContent } from "@nextui-org/react";
 import { PaginationState, SortingState, getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import { useEffect, useMemo, useState } from "react";
 import { CreateSpeciesForm } from "./CrudSpeciesForm";
-import { CustomTable } from "../taxonomy/sections/forms/CustomTable";
+import { CustomTable } from "@/components/CustomTable";
 import { Species } from "@/interfaces/species.interface";
 import { columns } from './columns';
+import { SearchIcon } from "lucide-react";
 
 
 const AdminSpeciesPage = () => {
@@ -22,6 +23,12 @@ const AdminSpeciesPage = () => {
   const [sorting, setSorting] = useState<SortingState>([
     { id: 'scientificName', desc: false },
   ]);
+  const [searchKey, setSearchKey] = useState<string>('');
+
+  const handleSearch = (event: any) => {
+    setPagination({ pageIndex: 0, pageSize });
+    setSearchKey(event.target.value)
+  }
 
   const pagination = useMemo(
     () => ({
@@ -46,6 +53,7 @@ const AdminSpeciesPage = () => {
         orderBy: sorting.length === 1 ? sorting[0].id : undefined,
         orderDirection:
           sorting.length === 1 ? (sorting[0].desc ? 'DESC' : 'ASC') : undefined,
+        searchKey: searchKey
       },
     },
     { keepPreviousData: true }
@@ -86,53 +94,79 @@ const AdminSpeciesPage = () => {
 
   return (
     <RolRequiredPageWrapper roles={[Role.ADMIN, Role.EDITOR]}>
-
       <PageTitle title='Especies' />
 
-      <div className='flex flex-row justify-between'>
-        <PageSubTitle title='Listado de especies' />
-        <Button
-          color={openCreate ? 'danger' : 'success'}
-          radius="sm"
-          className="uppercase text-white"
-          onClick={toggleCreateForm}
-        >
-          {openCreate ? 'Cancelar' : 'Crear'}
-        </Button>
-      </div>
+      <div className='p-3'>
+        <hr />
+        <div className="grid grid-cols-12 gap-2 mt-2">
+          <div className="col-span-12 md:col-span-4 order-1 flex justify-center md:justify-start items-center">
+            <PageSubTitle title='Listado de especies' className='mb-0' />
+          </div>
 
-      <br />
+          <div className="col-span-12 md:col-span-4 order-3 md:order-2 flex justify-center items-center">
+            <Input
+              // Value
+              type="search"
+              value={searchKey}
+              onChange={handleSearch}
+              placeholder="Buscar..."
+              // Style
+              classNames={{
+                base: "max-w-full h-8",
+                mainWrapper: "h-full",
+                input: "text-small",
+                inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+              }}
+              size="sm"
+              startContent={<SearchIcon size={18} />}
+            />
+          </div>
 
-      <Modal
-        size="5xl"
-        radius="sm"
-        isOpen={openCreate}
-        onClose={() => setOpenCreate(false)}
-        isDismissable={false}
-        scrollBehavior="outside"
-      >
-        <ModalThemeWrapper>
-          <ModalContent>
-            <div className='p-5 bg-light dark:bg-dark'>
-              <CreateSpeciesForm toggleVisibility={setOpenCreate} />
-            </div>
-          </ModalContent>
-        </ModalThemeWrapper>
-      </Modal>
-
-      {getSpeciesIsError && <p className='text-danger'>Error...</p>}
-
-      {getSpeciesIsLoading && (
-        <div className='flex justify-center'>
-          <CircularProgress aria-label="loading" />
+          <div className="col-span-12 md:col-span-4 order-2 md:order-3 flex justify-center md:justify-end items-center">
+            <Button
+              color={openCreate ? 'danger' : 'success'}
+              radius="sm"
+              className="uppercase text-white w-full md:w-auto h-8"
+              onClick={toggleCreateForm}
+            >
+              {openCreate ? 'Cancelar' : 'Nuevo'}
+            </Button>
+          </div>
         </div>
-      )}
 
-      {getSpeciesIsSuccess && (
-        <section id='species' className='species'>
-          <CustomTable table={table} />
-        </section>
-      )}
+        <br />
+
+        <Modal
+          size="5xl"
+          radius="sm"
+          isOpen={openCreate}
+          onClose={() => setOpenCreate(false)}
+          isDismissable={false}
+          scrollBehavior="outside"
+        >
+          <ModalThemeWrapper>
+            <ModalContent>
+              <div className='p-5 bg-light dark:bg-dark'>
+                <CreateSpeciesForm toggleVisibility={setOpenCreate} />
+              </div>
+            </ModalContent>
+          </ModalThemeWrapper>
+        </Modal>
+
+        {getSpeciesIsError && <p className='text-danger'>Error...</p>}
+
+        {getSpeciesIsLoading && (
+          <div className='flex justify-center'>
+            <CircularProgress aria-label="loading" />
+          </div>
+        )}
+
+        {getSpeciesIsSuccess && (
+          <section id='species' className='species'>
+            <CustomTable table={table} totalItems={getSpeciesData.meta.totalItems ?? 0} />
+          </section>
+        )}
+      </div>
     </RolRequiredPageWrapper>
   );
 }
